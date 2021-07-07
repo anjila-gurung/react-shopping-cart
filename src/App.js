1c7807050dc53abc528e838a685efc38eda230d6
AnjilaGurung <anjilagurung@prixatech.com>
2021-07-07 23:11:46 +0545

    product component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 //feature-branch
-//i cant see changes here
-//ok helpo
 import React from 'react';
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        All right is reserve
-      </footer>
-    </div>
-  );
+import Products from './components/Products';
+import data from './data.json';
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size:"",
+      sort:"",
+
+    };
+  }
+  render(){  
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+         <div className="content">
+           <div className="main">
+           <Products products={this.state.products}/>
+           </div>
+           <div className="sidebar">
+            cart Items
+           </div>
+         </div>
+        </main>
+        <footer>
+          All right is reserve
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
